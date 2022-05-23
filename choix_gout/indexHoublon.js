c3fd63e2cf26d555985f9e607ac7d36b14f83967
JeannePacard <97599809+JeannePacard@users.noreply.github.com>
2022-05-23 23:38:31 +0200

    Update indexHoublon.js

--- choix_gout/indexHoublon.js
+++ choix_gout/indexHoublon.js
@@ -456,7 +456,7 @@ if (reversed == null) { reversed = false; }
 		/*
 		Charge l’URL dans une nouvelle fenêtre du navigateur.
 		*/
-		window.open('/Isis/choix_gout/indexRefroidissement.html', '_self');
+		window.open('/IsisFinale/choix_gout/indexRefroidissement.html', '_self');
 		});
 	}
 
