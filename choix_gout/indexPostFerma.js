672c19074f90bf5cba72eb2cfbd7233e784df7ca
JeannePacard <97599809+JeannePacard@users.noreply.github.com>
2022-05-23 11:04:37 +0200

    Update indexPostFerma.js

--- choix_gout/indexPostFerma.js
+++ choix_gout/indexPostFerma.js
@@ -460,7 +460,7 @@ if (reversed == null) { reversed = false; }
 		/*
 		Charge l’URL dans une nouvelle fenêtre du navigateur.
 		*/
-		window.open('/choix_gout/indexEmbouteillage.html', '_self');
+		window.open('/Isis/choix_gout/indexEmbouteillage.html', '_self');
 		});
 	}
 	this.frame_35 = function() {
@@ -468,7 +468,7 @@ if (reversed == null) { reversed = false; }
 		/*
 		Charge l’URL dans une nouvelle fenêtre du navigateur.
 		*/
-		window.open('indexFermentation.html','_self');
+		window.open('/Isis/choix_gout/indexFermentation.html','_self');
 	}
 
 	// actions tween:
